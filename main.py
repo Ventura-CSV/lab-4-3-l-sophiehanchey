4609e8ccbb2182c8c1147870d21f26fdcfe59276
Kyu Lee <kwooler@gmail.com>
2023-05-22 23:28:21 -0700

    Lab 4-3

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    total = 0
+
+    numbers = [0] * 5
+    for i in range(len(numbers)):
+        numbers[i] = int(input('Enter a value: '))
+
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+    # total = sum(numbers)
+    print(total)
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return total, numbers
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,18 +10,23 @@ def test_main_1():
     datastr = '5\n2\n3\n1\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret, numbers = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    assert ret == 16
+    target = [5, 2, 3, 1, 5]
+    assert len(numbers) == 5
+    assert numbers == target
+
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*16[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*16[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
@@ -30,18 +35,23 @@ def test_main_2():
     datastr = '1\n2\n3\n4\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret, numbers = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    assert ret == 15
+    target = [1, 2, 3, 4, 5]
+    assert len(numbers) == 5
+    assert numbers == target
+
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*15[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*15[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_3():
@@ -50,15 +60,19 @@ def test_main_3():
     datastr = '10\n20\n30\n40\n50'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret, numbers = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
+    target = [10, 20, 30, 40, 50]
+    assert ret == 150
+    assert len(numbers) == 5
+    assert numbers == target
 
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*150[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*150[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
