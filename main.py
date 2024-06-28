88e16238b416ce224cbe5ccdeba79bbdb2719369
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 12:00:44 -0700

    intial code, error in size of list

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ def main():
     for i in range(len(numbers)):
         numbers[i] = int(input('Enter a value: '))
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    for i in numbers:
+        total += numbers[i]
 
     # total = sum(numbers)
     print(total)
